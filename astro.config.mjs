6a48913b8e4ca17d8daed6509b6800f47bfd4336
EzequielMonteleone <monteleone.ezequiel@gmail.com>
2026-06-12 15:21:17 -0300

    config del site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ezequielmonteleone.vercel.app',
+  site: 'https://portafolio-tawny-five-16.vercel.app/',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
